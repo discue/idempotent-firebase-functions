561b41e6ecc80dc08591f958586a1026f59d2461
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-09-02 11:15:30 +0200

    chore: set firebase project env vars

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 export NODE_ENV='ci'
 export FIREBASE_AUTH_EMULATOR_HOST=localhost:9099
+export FIREBASE_CONFIG='{ "projectId": "demo-dsq" }'
 export FIRESTORE_EMULATOR_HOST=localhost:9999
+export GCLOUD_PROJECT=demo-dsq
 
 npm run test
